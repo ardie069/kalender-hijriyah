95a76d8ac3cfc6a0fb38b780b79fd92cb380503c
ardie069 <ardi.pulaem069@gmail.com>
2025-04-24 04:32:03 +0700

    fix: Update import path for hijriCalculator to correct directory structure

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import serverless from 'serverless-http';
-import { getHijriDate, predictEndOfMonth } from '../hijriCalculator.js';
+import { getHijriDate, predictEndOfMonth } from '../backend/hijriCalculator.js';
 
 const app = express();
 
