4189e921067f9e62407c697f56102dfe61497816
ardie069 <ardi.pulaem069@gmail.com>
2025-04-23 20:05:22 +0700

    Add build configuration for Vite with ESNext target and ESBuild minification

--- kalender-hijriyah/vite.config.js
+++ kalender-hijriyah/vite.config.js
@@ -3,6 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+  },
   server: {
     proxy: {
       '/api': {
