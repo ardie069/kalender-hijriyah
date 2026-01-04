6f18c0c0ca6eb320cf6748320d3e3669065174bf
Ardiansyah <ardi.pulaem069@gmail.com>
2026-01-04 14:52:53 +0700

    fix: update ephemeris file auto download if not exists

--- apps/backend/app/deps/astronomy.py
+++ apps/backend/app/deps/astronomy.py
@@ -3,14 +3,17 @@ from skyfield.api import load  # type: ignore
 
 BASE_DIR = Path(__file__).resolve().parents[2]
 DATA_DIR = BASE_DIR / "data"
+DATA_DIR.mkdir(exist_ok=True)
 
 EPHEMERIS_FILE = DATA_DIR / "de421.bsp"
 
-if not EPHEMERIS_FILE.exists():
-    raise FileNotFoundError(f"Ephemeris file not found: {EPHEMERIS_FILE}")
-
 ts = load.timescale()
-eph = load(str(EPHEMERIS_FILE))
+
+if EPHEMERIS_FILE.exists():
+    eph = load(str(EPHEMERIS_FILE))
+else:
+    eph = load("de421.bsp")
+    eph.save(str(EPHEMERIS_FILE))  # type: ignore
 
 earth = eph["earth"]
 sun = eph["sun"]
