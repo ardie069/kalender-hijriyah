7c25839e4bc8182e33964a90f791fe51282b5e64
Ardiansyah <ardi.pulaem069@gmail.com>
2026-01-11 21:24:01 +0700

    fix: load file bsp astronomy in "data" directory, and update logic code in astronomy.py

--- apps/backend/app/deps/astronomy.py
+++ apps/backend/app/deps/astronomy.py
@@ -1,19 +1,14 @@
 from pathlib import Path
-from skyfield.api import load  # type: ignore
+from skyfield.api import Loader
 
 BASE_DIR = Path(__file__).resolve().parents[2]
 DATA_DIR = BASE_DIR / "data"
 DATA_DIR.mkdir(exist_ok=True)
 
-EPHEMERIS_FILE = DATA_DIR / "de421.bsp"
+loader = Loader(str(DATA_DIR))
 
-ts = load.timescale()
-
-if EPHEMERIS_FILE.exists():
-    eph = load(str(EPHEMERIS_FILE))
-else:
-    eph = load("data/de421.bsp")
-    eph.save(str(EPHEMERIS_FILE))  # type: ignore
+ts = loader.timescale()
+eph = loader("de421.bsp")
 
 earth = eph["earth"]
 sun = eph["sun"]
