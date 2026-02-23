ac28f4e4c9fa450f7828a941864015fe2cc85a95
ardie069 <ardi.pulaem069@gmail.com>
2026-02-23 20:35:33 +0700

    fix: optimation of method factory

--- apps/backend/app/core/method_factory.py
+++ apps/backend/app/core/method_factory.py
@@ -1,15 +1,23 @@
 from .methods.umm_al_qura import UmmAlQuraMethod
 from .methods.local_hisab import LocalHisabMethod
 from .methods.local_rukyat import LocalRukyatMethod
-from .methods.ughc import UGHCMethod
+from .methods.ughc_base import BaseUGHCMethod
+
+_instances = {}
 
 
 def get_method_instance(method: str):
-    mapping = {
-        "umm_al_qura": UmmAlQuraMethod(),
-        "local_hisab": LocalHisabMethod(),
-        "local_rukyat": LocalRukyatMethod(),
-        "ughc": UGHCMethod(),
+    classes = {
+        "umm_al_qura": UmmAlQuraMethod,
+        "local_hisab": LocalHisabMethod,
+        "local_rukyat": LocalRukyatMethod,
+        "ughc": BaseUGHCMethod,
     }
 
-    return mapping[method]
+    if method not in classes:
+        raise ValueError(f"Metode '{method}' tidak ditemukan.")
+
+    if method not in _instances:
+        _instances[method] = classes[method]()
+
+    return _instances[method]
